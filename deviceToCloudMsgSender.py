c80d021f06ba749378da92c3b96e72ea20c17d5b
HoorishIqbal <34625692+HoorishIqbal@users.noreply.github.com>
2018-01-23 07:54:21 -0800

    Create deviceToCloudMsgSender.py

---
+++ deviceToCloudMsgSender.py
@@ -0,0 +1,45 @@
+import base64
+import hmac
+import hashlib
+import time
+import requests
+import urllib
+import os
+
+class DeviceToCloudMsgSender:
+    
+    API_VERSION = '2016-02-03'
+    TOKEN_VALID_SECS = 10
+    TOKEN_FORMAT = 'SharedAccessSignature sig=%s&se=%s&skn=%s&sr=%s'
+    
+    def __init__(self, connectionString=None):
+        if connectionString != None:
+            #splitting connectionstring by ' ; ' and assign respective values to variables by ' = '
+            iotHost, keyName, keyValue = [sub[sub.index('=') + 1:] for sub in connectionString.split(";")]
+            self.iotHost = iotHost
+            self.keyName = keyName
+            self.keyValue = keyValue
+            
+    def _buildExpiryOn(self):
+        return '%d' % (time.time() + self.TOKEN_VALID_SECS)
+    
+    def _buildIoTHubSasToken(self, deviceId):
+        resourceUri = '%s/devices/%s' % (self.iotHost, deviceId)
+        targetUri = resourceUri.lower()
+        expiryTime = self._buildExpiryOn()
+        toSign = '%s\n%s' % (targetUri, expiryTime)
+        key = base64.b64decode(self.keyValue.encode('utf-8'))
+        signature = urllib.quote_plus(
+            base64.b64encode(
+                hmac.HMAC(key, toSign.encode('utf-8'), hashlib.sha256).digest()
+            )
+        ).replace('/', '%2F')
+        print targetUri
+        return self.TOKEN_FORMAT % (signature, expiryTime, self.keyName, targetUri)
+    
+    def sendD2CMsg(self, deviceId, message):
+        sasToken = self._buildIoTHubSasToken(deviceId)
+        url = 'https://%s/devices/%s/messages/events?api-version=%s' % (self.iotHost, deviceId, self.API_VERSION)
+        r = requests.post(url, headers={'Authorization': sasToken, 'Content-Type': 'application/json'}, data=message)
+        #print url
+        return r.text, r.status_code, message
